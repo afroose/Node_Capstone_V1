fdb6c9cb0996d0048ea22059717da01cde366423
afroose <arnaud.roose@gmail.com>
2017-06-07 17:05:12 -0400

    Adds catch error - dishRouter.js - for POST Heroku test

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -20,16 +20,41 @@ router.get('/dish', (req,res) => {
     .then(dishes => {
       res.json(dishes);
     });
-})
+});
+
+// router.post('/dish', (req, res) => {
+//   Dishids
+//     .create({
+//       dishname: req.body.dishname,
+//       content: req.body.content
+//     })
+//     .then(
+//       dishes => res.status(201).json(dishes)
+//     );
+// });
 
 router.post('/dish', (req, res) => {
+  const requiredFields = ['dishname', 'content'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
   Dishids
     .create({
-      name: req.body.name
+      dishname: req.body.dishname,
+      content: req.body.content
     })
-    .then(
-      dishes => res.status(201).json(dishes)
-    )
-})
+    .then(dishes => res.status(201).json(dishes))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'Something went wrong'});
+    });
+
+});
 
 module.exports = router;
