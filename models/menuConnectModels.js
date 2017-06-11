dccb8b7a94ffec9c6000dcf25b9c2a03de99c38f
afroose <arnaud.roose@gmail.com>
2017-06-11 11:44:47 -0400

    Adds refs for populate to menuConnectRouter.js

--- models/menuConnectModels.js
+++ models/menuConnectModels.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 // this is our schema to connect menus and locations
 const menuConnectSchema = mongoose.Schema({
   dish_id: {type: String, required: true},
-  location_id: {type: String, required: true}
+  location_id: {type: String, required: true},
+  locations: [{type: mongoose.Schema.Types.ObjectId, ref: 'Locationid' }],
+  dishes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Dishids' }]
 });
 
 // note that all instance methods and virtual properties on our
