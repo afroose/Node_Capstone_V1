3b0881802df7e73bf0252ac4cae7d3891d50e43c
afroose <arnaud.roose@gmail.com>
2017-07-17 18:58:00 -0400

    Points to mLab database

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://localhost/menu-select-app';
+// exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://localhost/menu-select-app';
+exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://MasterChef:4Tune8!!@ds111622.mlab.com:11622/menu-select-app';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/menu-select-test-app');
--- test/test-location-integration.js
+++ test/test-location-integration.js
@@ -3,8 +3,6 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
-// this makes the should syntax available throughout
-// this module
 const should = chai.should();
 
 const {locationId} = require('../models/locationModels');
@@ -12,35 +10,22 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config/config');
 
 chai.use(chaiHttp);
-
-// used to put randomish documents in db
-// so we have data to work with and assert about.
-// we use the Faker library to automatically
-// generate placeholder values for author, title, content
-// and then we insert that data into mongo
 function seedLocationData() {
-  console.info('seeding Location data');
   const seedData = [];
 
   for (let i=1; i<=10; i++) {
     seedData.push(generateLocationData());
   }
-  console.log("hello test data")
-  console.log(seedData)
-  // this will return a promise
+  // console.log(seedData)
   return locationId.insertMany(seedData);
 }
 
-// used to generate data to put in db
 function generateLocationName() {
   const locationName = [
     'Manhattan', 'Queens', 'Brooklyn', 'Bronx', 'Staten Island'];
   return locationName[Math.floor(Math.random() * locationName.length)];
 }
 
-// generate an object represnting a restaurant.
-// can be used to generate seed data for db
-// or request.body data
 function generateLocationData() {
   return {
     locationName: generateLocationName(),
@@ -54,21 +39,13 @@ function generateLocationData() {
   }
 }
 
-// this function deletes the entire database.
-// we'll call it in an `afterEach` block below
-// to ensure  ata from one test does not stick
-// around for next one
 function tearDownDb() {
     console.warn('Deleting database');
-    // return mongoose.connection.dropDatabase();
+    return mongoose.connection.dropDatabase();
 }
 
 describe('Locations API resource', function() {
 
-  // we need each of these hook functions to return a promise
-  // otherwise we'd need to call a `done` callback. `runServer`,
-  // `seedRestaurantData` and `tearDownDb` each return a promise,
-  // so we return the value returned by these function calls.
   before(function() {
     console.log("test database" + TEST_DATABASE_URL)
     return runServer(TEST_DATABASE_URL);
@@ -98,9 +75,6 @@ describe('Locations API resource', function() {
       //    2. prove res has right status, data type
       //    3. prove the number of locations we got back is equal to number
       //       in db.
-      //
-      // need to have access to mutate and access `res` across
-      // `.then()` calls below, so declare it here so can modify in place
       let res;
       return chai.request(app)
         .get('/location')
@@ -116,7 +90,6 @@ describe('Locations API resource', function() {
           res.body.locations.should.have.length.of(count);
         });
     });
-
 
     it('should return locations with right fields', function() {
       // Strategy: Get back all restaurants, and ensure they have expected keys
@@ -146,4 +119,5 @@ describe('Locations API resource', function() {
         });
     });
   });
+
 });
